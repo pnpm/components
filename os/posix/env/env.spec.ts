38662c2d112750f4aeeaac73ea764a3a87c44f57
Zoltan Kochan <z@kochan.io>
2022-05-25 23:54:56 +0300

    feat: add os/posix/env component

---
+++ os/posix/env/env.spec.ts
@@ -0,0 +1,261 @@
+import fs from 'fs'
+import path from 'path'
+
+import { homedir } from 'os'
+import { tempDir } from '@pnpm/prepare'
+import { addDirToPosixEnvPath } from './env'
+
+jest.mock('os', () => {
+  const os = jest.requireActual('os')
+  return {
+    ...os,
+    homedir: jest.fn(),
+  }
+})
+
+let homeDir!: string
+let pnpmHomeDir!: string
+
+beforeEach(() => {
+  homeDir = tempDir()
+  pnpmHomeDir = path.join(homeDir, '.pnpm')
+  homedir['mockReturnValue'](homeDir)
+})
+
+describe('Bash', () => {
+  beforeAll(() => {
+    process.env.SHELL = '/bin/bash'
+  })
+  it('should append to empty shell script', async () => {
+    fs.writeFileSync('.bashrc', '', 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Appended new /)
+    const bashRCContent = fs.readFileSync('.bashrc', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`)
+  })
+  it('should put the new directory to the end of the PATH', async () => {
+    fs.writeFileSync('.bashrc', '', 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+      position: 'end',
+    })
+    expect(output).toMatch(/^Appended new /)
+    const bashRCContent = fs.readFileSync('.bashrc', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PATH:$PNPM_HOME"
+# pnpm end`)
+  })
+  it('should create a shell script', async () => {
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Created /)
+    const bashRCContent = fs.readFileSync('.bashrc', 'utf8')
+    expect(bashRCContent).toEqual(`# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`)
+  })
+  it('should make no changes to a shell script that already has the necessary configurations', async () => {
+    fs.writeFileSync('.bashrc', `
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`, 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Configuration already up-to-date /)
+    const bashRCContent = fs.readFileSync('.bashrc', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`)
+  })
+  it('should fail if the shell already has PNPM_HOME set to a different directory', async () => {
+    fs.writeFileSync('.bashrc', `
+# pnpm
+export PNPM_HOME="pnpm_home"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`, 'utf8')
+    await expect(
+      addDirToPosixEnvPath(pnpmHomeDir, {
+        proxyVarName: 'PNPM_HOME',
+        configSectionName: 'pnpm',
+      })
+    ).rejects.toThrowError(/The config file at/)
+  })
+  it('should not fail if setup is forced', async () => {
+    fs.writeFileSync('.bashrc', `
+# pnpm
+export PNPM_HOME="pnpm_home"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`, 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      overwriteProxyVar: true,
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Replaced /)
+    const bashRCContent = fs.readFileSync('.bashrc', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`)
+  })
+})
+
+describe('Zsh', () => {
+  beforeAll(() => {
+    process.env.SHELL = '/bin/zsh'
+  })
+  it('should append to empty shell script', async () => {
+    fs.writeFileSync('.zshrc', '', 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Appended new /)
+    const bashRCContent = fs.readFileSync('.zshrc', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`)
+  })
+  it('should make no changes to a shell script that already has the necessary configurations', async () => {
+    fs.writeFileSync('.zshrc', `
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`, 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Configuration already up-to-date /)
+    const bashRCContent = fs.readFileSync('.zshrc', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+export PNPM_HOME="${pnpmHomeDir}"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end`)
+  })
+})
+
+describe('Fish', () => {
+  beforeAll(() => {
+    process.env.SHELL = '/bin/fish'
+  })
+  it('should append to empty shell script', async () => {
+    fs.mkdirSync('.config/fish', { recursive: true })
+    fs.writeFileSync('.config/fish/config.fish', '', 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Appended new /)
+    const bashRCContent = fs.readFileSync('.config/fish/config.fish', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+set -gx PNPM_HOME "${pnpmHomeDir}"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`)
+  })
+  it('should add the new dir to the end of PATH', async () => {
+    fs.mkdirSync('.config/fish', { recursive: true })
+    fs.writeFileSync('.config/fish/config.fish', '', 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+      position: 'end',
+    })
+    expect(output).toMatch(/^Appended new /)
+    const bashRCContent = fs.readFileSync('.config/fish/config.fish', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+set -gx PNPM_HOME "${pnpmHomeDir}"
+set -gx PATH $PATH "$PNPM_HOME"
+# pnpm end`)
+  })
+  it('should create a shell script', async () => {
+    fs.mkdirSync('.config/fish', { recursive: true })
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Created /)
+    const bashRCContent = fs.readFileSync('.config/fish/config.fish', 'utf8')
+    expect(bashRCContent).toEqual(`# pnpm
+set -gx PNPM_HOME "${pnpmHomeDir}"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`)
+  })
+  it('should make no changes to a shell script that already has the necessary configurations', async () => {
+    fs.mkdirSync('.config/fish', { recursive: true })
+    fs.writeFileSync('.config/fish/config.fish', `
+# pnpm
+set -gx PNPM_HOME "${pnpmHomeDir}"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`, 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Configuration already up-to-date /)
+    const bashRCContent = fs.readFileSync('.config/fish/config.fish', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+set -gx PNPM_HOME "${pnpmHomeDir}"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`)
+  })
+  it('should fail if the shell already has PNPM_HOME set to a different directory', async () => {
+    fs.mkdirSync('.config/fish', { recursive: true })
+    fs.writeFileSync('.config/fish/config.fish', `
+# pnpm
+set -gx PNPM_HOME "pnpm_home"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`, 'utf8')
+    await expect(
+      addDirToPosixEnvPath(pnpmHomeDir, {
+        proxyVarName: 'PNPM_HOME',
+        configSectionName: 'pnpm',
+      })
+    ).rejects.toThrowError(/The config file at/)
+  })
+  it('should not fail if setup is forced', async () => {
+    fs.mkdirSync('.config/fish', { recursive: true })
+    fs.writeFileSync('.config/fish/config.fish', `
+# pnpm
+set -gx PNPM_HOME "pnpm_home"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`, 'utf8')
+    const output = await addDirToPosixEnvPath(pnpmHomeDir, {
+      proxyVarName: 'PNPM_HOME',
+      overwriteProxyVar: true,
+      configSectionName: 'pnpm',
+    })
+    expect(output).toMatch(/^Replaced /)
+    const bashRCContent = fs.readFileSync('.config/fish/config.fish', 'utf8')
+    expect(bashRCContent).toEqual(`
+# pnpm
+set -gx PNPM_HOME "${pnpmHomeDir}"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end`)
+  })
+})
---
+++ os/posix/env/env.ts
@@ -0,0 +1,153 @@
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+import PnpmError from '@pnpm/error'
+
+class BadShellSectionError extends PnpmError {
+  public current: string
+  public wanted: string
+  constructor ({ wanted, current, configFile }: { wanted: string, current: string, configFile: string }) {
+    super('BAD_SHELL_SECTION', `The config file at "${configFile} already contains a pnpm section but with other configuration`)
+    this.current = current
+    this.wanted = wanted
+  }
+}
+
+export type AddingPosition = 'start' | 'end'
+
+export interface AddDirToPosixEnvPathOpts {
+  proxyVarName?: string
+  overwriteProxyVar?: boolean
+  position?: AddingPosition
+  configSectionName: string
+}
+
+export async function addDirToPosixEnvPath (
+  dir: string,
+  opts: AddDirToPosixEnvPathOpts
+) {
+  const currentShell = detectCurrentShell()
+  return await updateShell(currentShell, dir, opts)
+}
+
+function detectCurrentShell () {
+  if (process.env.ZSH_VERSION) return 'zsh'
+  if (process.env.BASH_VERSION) return 'bash'
+  if (process.env.FISH_VERSION) return 'fish'
+  return typeof process.env.SHELL === 'string' ? path.basename(process.env.SHELL) : null
+}
+
+async function updateShell (
+  currentShell: string | null,
+  pnpmHomeDir: string,
+  opts: AddDirToPosixEnvPathOpts
+): Promise<string> {
+  switch (currentShell) {
+  case 'bash':
+  case 'zsh': {
+    return reportShellChange(await setupShell(currentShell, pnpmHomeDir, opts))
+  }
+  case 'fish': {
+    return reportShellChange(await setupFishShell(pnpmHomeDir, opts))
+  }
+  }
+  return 'Could not infer shell type.'
+}
+
+function reportShellChange ({ action, configFile }: ShellSetupResult): string {
+  switch (action) {
+  case 'created': return `Created ${configFile}`
+  case 'added': return `Appended new lines to ${configFile}`
+  case 'updated': return `Replaced configuration in ${configFile}`
+  case 'skipped': return `Configuration already up-to-date in ${configFile}`
+  }
+}
+
+type ShellSetupAction = 'created' | 'added' | 'updated' | 'skipped'
+
+interface ShellSetupResult {
+  configFile: string
+  action: ShellSetupAction
+}
+
+async function setupShell (shell: 'bash' | 'zsh', dir: string, opts: AddDirToPosixEnvPathOpts): Promise<ShellSetupResult> {
+  const configFile = path.join(os.homedir(), `.${shell}rc`)
+  let content!: string
+  const _createPathValue = createPathValue.bind(null, opts.position ?? 'start')
+  if (opts.proxyVarName) {
+    content = `# ${opts.configSectionName}
+export ${opts.proxyVarName}="${dir}"
+export PATH="${_createPathValue(`$${opts.proxyVarName}`)}"
+# ${opts.configSectionName} end`
+  } else {
+    content = `# ${opts.configSectionName}
+export PATH="${_createPathValue(dir)}"
+# ${opts.configSectionName} end`
+  }
+  return {
+    action: await updateShellConfig(configFile, content, opts),
+    configFile,
+  }
+}
+
+function createPathValue (position: AddingPosition, dir: string) {
+  return position === 'start'
+    ? `${dir}:$PATH`
+    : `$PATH:${dir}`
+}
+
+async function setupFishShell (dir: string, opts: AddDirToPosixEnvPathOpts): Promise<ShellSetupResult> {
+  const configFile = path.join(os.homedir(), '.config/fish/config.fish')
+  let content!: string
+  const _createPathValue = createFishPathValue.bind(null, opts.position ?? 'start')
+  if (opts.proxyVarName) {
+    content = `# ${opts.configSectionName}
+set -gx ${opts.proxyVarName} "${dir}"
+set -gx PATH ${_createPathValue(`$${opts.proxyVarName}`)}
+# ${opts.configSectionName} end`
+  } else {
+    content = `# ${opts.configSectionName}
+set -gx PATH ${_createPathValue(dir)}
+# ${opts.configSectionName} end`
+  }
+  return {
+    action: await updateShellConfig(configFile, content, opts),
+    configFile,
+  }
+}
+
+function createFishPathValue (position: AddingPosition, dir: string) {
+  return position === 'start'
+    ? `"${dir}" $PATH`
+    : `$PATH "${dir}"`
+}
+
+async function updateShellConfig (
+  configFile: string,
+  newContent: string,
+  opts: AddDirToPosixEnvPathOpts
+): Promise<ShellSetupAction> {
+  if (!fs.existsSync(configFile)) {
+    await fs.promises.writeFile(configFile, newContent, 'utf8')
+    return 'created'
+  }
+  const configContent = await fs.promises.readFile(configFile, 'utf8')
+  const match = configContent.match(new RegExp(`# ${opts.configSectionName}[\\s\\S]*# ${opts.configSectionName} end`, 'g'))
+  if (!match) {
+    await fs.promises.appendFile(configFile, `\n${newContent}`, 'utf8')
+    return 'added'
+  }
+  if (match[0] !== newContent) {
+    if (!opts.overwriteProxyVar) {
+      throw new BadShellSectionError({ current: match[1], wanted: newContent, configFile })
+    }
+    const newConfigContent = replaceSection(configContent, newContent, opts.configSectionName)
+    await fs.promises.writeFile(configFile, newConfigContent, 'utf8')
+    return 'updated'
+  }
+  return 'skipped'
+}
+
+function replaceSection (originalContent: string, newSection: string, sectionName: string): string {
+  return originalContent.replace(new RegExp(`# ${sectionName}[\\s\\S]*# ${sectionName} end`, 'g'), newSection)
+}
