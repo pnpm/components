f2a318ecfc4a4d4643dd3ef1196ef623ae174b5d
Zoltan Kochan <z@kochan.io>
2023-04-04 21:54:37 +0300

    refactor: env-replace

--- config/env-replace/env-replace.ts
+++ config/env-replace/env-replace.ts
@@ -8,23 +8,21 @@ function replaceEnvMatch (env: NodeJS.ProcessEnv, orig: string, escape: string, 
   if (escape.length % 2) {
     return orig.slice((escape.length + 1) / 2)
   }
-
-  
-  const matched = name.match(/([^:-]+)(:?)-(.+)/)
-  let envValue;
-  if (matched) {
-    const [, variableName, colon, fallback] = matched
-    if (Object.prototype.hasOwnProperty.call(env, variableName)) {
-      envValue = !env[variableName] && colon ? fallback : env[variableName]
-    } else {
-      envValue = fallback
-    }
-  } else {
-    envValue = env[name]
-  }
-
+  const envValue = getEnvValue(env, name)
   if (envValue === undefined) {
     throw new Error(`Failed to replace env in config: ${orig}`)
   }
   return `${(escape.slice(escape.length / 2))}${envValue}`
+}
+
+const ENV_VALUE = /([^:-]+)(:?)-(.+)/
+
+function getEnvValue (env: NodeJS.ProcessEnv, name: string): string {
+  const matched = name.match(ENV_VALUE)
+  if (!matched) return env[name]
+  const [, variableName, colon, fallback] = matched
+  if (Object.prototype.hasOwnProperty.call(env, variableName)) {
+    return !env[variableName] && colon ? fallback : env[variableName]
+  }
+  return fallback
 }
