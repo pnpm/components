ac0ee55a2ad2f4c8798a24d2766b476ad8512f2f
Zoltan Kochan <z@kochan.io>
2023-10-06 02:06:35 +0300

    feat: specifying allowed build list via a json file

--- builder/policy/policy.ts
+++ builder/policy/policy.ts
@@ -1,15 +1,23 @@
+import fs from 'fs'
+
 export function createAllowBuildFunction (
   opts: {
     neverBuiltDependencies?: string[]
     onlyBuiltDependencies?: string[]
+    onlyBuiltDependenciesFile?: string
   }
 ): undefined | ((pkgName: string) => boolean) {
+  if (opts.onlyBuiltDependenciesFile || opts.onlyBuiltDependencies != null) {
+    const onlyBuiltDeps = opts.onlyBuiltDependencies ?? []
+    if (opts.onlyBuiltDependenciesFile) {
+      onlyBuiltDeps.push(...JSON.parse(fs.readFileSync(opts.onlyBuiltDependenciesFile, 'utf8')))
+    }
+    const onlyBuiltDependencies = new Set(onlyBuiltDeps)
+    return (pkgName) => onlyBuiltDependencies.has(pkgName)
+  }
   if (opts.neverBuiltDependencies != null && opts.neverBuiltDependencies.length > 0) {
     const neverBuiltDependencies = new Set(opts.neverBuiltDependencies)
     return (pkgName) => !neverBuiltDependencies.has(pkgName)
-  } else if (opts.onlyBuiltDependencies != null) {
-    const onlyBuiltDependencies = new Set(opts.onlyBuiltDependencies)
-    return (pkgName) => onlyBuiltDependencies.has(pkgName)
   }
   return undefined
 }
