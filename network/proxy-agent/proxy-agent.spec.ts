19e788ab8509b2312556a30b3f52d86d48df5c96
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-02-06 20:18:44 +0100

    chore: fix TS errors

--- network/proxy-agent/proxy-agent.spec.ts
+++ network/proxy-agent/proxy-agent.spec.ts
@@ -56,7 +56,7 @@ test('a socks proxy', () => {
   }
   const agent = getProxyAgent('https://foo.com/bar', opts)
   expect(agent instanceof SocksProxyAgent).toBeTruthy()
-  expect(agent['proxy']).toEqual({
+  expect((agent instanceof SocksProxyAgent)['proxy']).toEqual({
     host: 'my.proxy',
     port: 1234,
     type: 5,
