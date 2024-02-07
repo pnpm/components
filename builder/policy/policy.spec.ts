9336d624331f0da679526800bb7ebc63865f7129
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2024-02-07 11:57:02 +0100

    style: revert formatter changes

--- builder/policy/policy.spec.ts
+++ builder/policy/policy.spec.ts
@@ -1,54 +1,54 @@
-import path from 'path';
-import { createAllowBuildFunction } from './policy';
+import path from 'path'
+import { createAllowBuildFunction } from './policy'
 
 it('should neverBuiltDependencies', () => {
   const allowBuild = createAllowBuildFunction({
     neverBuiltDependencies: ['foo'],
-  });
-  expect(typeof allowBuild).toBe('function');
+  })
+  expect(typeof allowBuild).toBe('function')
   if (allowBuild) {
-    expect(allowBuild('foo')).toBeFalsy();
-    expect(allowBuild('bar')).toBeTruthy();
+    expect(allowBuild('foo')).toBeFalsy()
+    expect(allowBuild('bar')).toBeTruthy()
   }
-});
+})
 
 it('should onlyBuiltDependencies', () => {
   const allowBuild = createAllowBuildFunction({
     onlyBuiltDependencies: ['foo'],
-  });
-  expect(typeof allowBuild).toBe('function');
+  })
+  expect(typeof allowBuild).toBe('function')
   if (allowBuild) {
-    expect(allowBuild('foo')).toBeTruthy();
-    expect(allowBuild('bar')).toBeFalsy();
+    expect(allowBuild('foo')).toBeTruthy()
+    expect(allowBuild('bar')).toBeFalsy()
   }
-});
+})
 
 it('should onlyBuiltDependencies set via a file', () => {
   const allowBuild = createAllowBuildFunction({
     onlyBuiltDependenciesFile: path.join(__dirname, 'onlyBuild.json'),
-  });
-  expect(typeof allowBuild).toBe('function');
+  })
+  expect(typeof allowBuild).toBe('function')
   if (allowBuild) {
-    expect(allowBuild('zoo')).toBeTruthy();
-    expect(allowBuild('qar')).toBeTruthy();
-    expect(allowBuild('bar')).toBeFalsy();
+    expect(allowBuild('zoo')).toBeTruthy()
+    expect(allowBuild('qar')).toBeTruthy()
+    expect(allowBuild('bar')).toBeFalsy()
   }
-});
+})
 
 it('should onlyBuiltDependencies set via a file and config', () => {
   const allowBuild = createAllowBuildFunction({
     onlyBuiltDependencies: ['bar'],
     onlyBuiltDependenciesFile: path.join(__dirname, 'onlyBuild.json'),
-  });
-  expect(typeof allowBuild).toBe('function');
+  })
+  expect(typeof allowBuild).toBe('function')
   if (allowBuild) {
-    expect(allowBuild('zoo')).toBeTruthy();
-    expect(allowBuild('qar')).toBeTruthy();
-    expect(allowBuild('bar')).toBeTruthy();
-    expect(allowBuild('esbuild')).toBeFalsy();
+    expect(allowBuild('zoo')).toBeTruthy()
+    expect(allowBuild('qar')).toBeTruthy()
+    expect(allowBuild('bar')).toBeTruthy()
+    expect(allowBuild('esbuild')).toBeFalsy()
   }
-});
+})
 
 it('should return undefined if no policy is set', () => {
-  expect(createAllowBuildFunction({})).toBeUndefined();
-});
+  expect(createAllowBuildFunction({})).toBeUndefined()
+})
